423819a0659836b288fdd9e126da84e406ff504b
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-24 13:20:05 +0100

    update the login page

--- view/Login.php
+++ view/Login.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,45 +6,64 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
+<section class="p-4" style="background-color: #eee;height:100vh">
 
-<body>
-    <section class="d-flex justify-content-center align-items-center" style="height:100vh">
-        <form class="shadow-lg p-4" method="POST" action=" login.php">
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-                <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required />
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" style="border-radius: 25px;">
+                <div class="card-body p-md-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                            <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
+                            <form class="shadow-lg p-4" method="POST" action=" login.php">
+                                <!-- Email input -->
+                                <div class="form-outline mb-4">
+                                    <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required />
 
-            </div>
+                                </div>
 
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-                <input type="password" minlength="8" maxlength="20" name="password" id="password" class="form-control" placeholder="password" required />
-            </div>
+                                <!-- Password input -->
+                                <div class="form-outline mb-4">
+                                    <input type="password" minlength="8" maxlength="20" name="password" id="password" class="form-control" placeholder="password" required />
+                                </div>
 
-            <!-- 2 column grid layout for inline styling -->
-            <div class="row mb-4">
-                <div class="col d-flex justify-content-center">
-                    <!-- Checkbox -->
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                        <label class="form-check-label" for="form2Example31"> Remember me </label>
+                                <!-- 2 column grid layout for inline styling -->
+                                <div class="row mb-4">
+                                    <div class="col d-flex justify-content-center">
+                                        <!-- Checkbox -->
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                                            <label class="form-check-label" for="form2Example31"> Remember me </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <!-- Simple link -->
+                                        <a href="/www/signup.php">Not a user SignUp?</a>
+                                    </div>
+                                </div>
+
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-primary btn-block mb-4">Login</button>
+                                <?php isset($error) ? include("./view/Alert.php") : "" ?>
+
+
+
+
+                                <!-- Register buttons -->
+
+                            </form>
+
+                        </div>
+                        <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                            <img src="/www/view/assets/signup.jpg" class="img-fluid" alt="Sample image">
+
+                        </div>
                     </div>
                 </div>
-
-                <div class="col">
-                    <!-- Simple link -->
-                    <a href="/www/signup.php">Not a user SignUp?</a>
-                </div>
             </div>
+        </div>
+    </div>
 
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-block mb-4">Login</button>
-            <?php isset($error) ? include("./view/Alert.php") : "" ?>
-
-
-
-
-            <!-- Register buttons -->
-
-        </form>
-        <section>
+</section>
